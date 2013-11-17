6c36a5bf9db12598d7f8f23d0bfa3a8b745364d5
Wayne Arthurton <warthurton@arthurton.com>
2013-11-17 13:23:05 -0500

    SignalR2 Workds

---
+++ ContactsSignalR/ContactsSignalR.HTMLClient/Screens/BrowseContacts.lsml.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+/// <reference path="../Scripts/jquery.signalR-2.0.0.js" />
+
+
+
+myapp.BrowseContacts.created = function (screen) {
+    // Write code here.
+    screen.updates = "Browse Contacts Screen Created";
+
+    $(function () {
+
+        contact = $.connection.contactHub;
+        contact.client.broadcastMessage = function (message) {
+            screen.updates = message;
+        };
+
+        $.connection.hub.start()
+        .done(function () {
+        })
+        .fail(function () {
+            alert("Could not Connect! - ensure EnableCrossDomain = true");
+        });
+    });
+};
---
+++ ContactsSignalR/ContactsSignalR.Server/DataSources/ApplicationData/_ApplicationDataService.lsml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+using Microsoft.LightSwitch.Security.Server;
+using Microsoft.AspNet.SignalR;
+
+namespace LightSwitchApplication
+{
+    public partial class ApplicationDataService
+    {
+
+        partial void Contacts_Updated(Contact entity)
+        {
+            string message = "A contact for " + entity.FirstName + " " + entity.LastName + " was just updated";
+            var context = GlobalHost.ConnectionManager.GetHubContext<ContactHub>();
+            context.Clients.All.broadcastMessage(message);
+        }
+
+        partial void Contacts_Inserted(Contact entity)
+        {
+            string message = "A contact for " + entity.FirstName + " " + entity.LastName + " was just created";
+            var context = GlobalHost.ConnectionManager.GetHubContext<ContactHub>();
+            context.Clients.All.broadcastMessage(message);
+        }
+    }
+}
